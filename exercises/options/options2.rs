9891c6ce45475dbdcebe0b926595b7e1767e7225
holyliao <holy468@live.com>
2024-04-28 13:38:29 +0000

    update

--- exercises/options/options2.rs
+++ exercises/options/options2.rs
@@ -3,7 +3,7 @@
 // Execute `rustlings hint options2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 #[cfg(test)]
 mod tests {
@@ -12,8 +12,8 @@ mod tests {
         let target = "rustlings";
         let optional_target = Some(target);
 
-        // TODO: Make this an if let statement whose value is "Some" type
-        word = optional_target {
+         // Use if let to handle the Some variant
+         if let Some(word) = optional_target {
             assert_eq!(word, target);
         }
     }
@@ -29,10 +29,8 @@ mod tests {
 
         let mut cursor = range;
 
-        // TODO: make this a while let statement - remember that vector.pop also
-        // adds another layer of Option<T>. You can stack `Option<T>`s into
-        // while let and if let.
-        integer = optional_integers.pop() {
+         // Use while let to handle nested Option<T>
+         while let Some(Some(integer)) = optional_integers.pop() {
             assert_eq!(integer, cursor);
             cursor -= 1;
         }
--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -20,7 +20,8 @@
 //
 // No hints this time!
 
-// I AM NOT DONE
+
+
 
 pub enum Command {
     Uppercase,
@@ -31,12 +32,21 @@ pub enum Command {
 mod my_module {
     use super::Command;
 
-    // TODO: Complete the function signature!
-    pub fn transformer(input: ???) -> ??? {
-        // TODO: Complete the output declaration!
-        let mut output: ??? = vec![];
-        for (string, command) in input.iter() {
-            // TODO: Complete the function body. You can do it!
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
+        let mut output: Vec<String> = vec![];
+        for (string, command) in input {
+            let transformed_string = match command {
+                Command::Uppercase => string.to_uppercase(),
+                Command::Trim => string.trim().to_string(),
+                Command::Append(n) => {
+                    let mut appended_string = string;
+                    for _ in 0..n {
+                        appended_string.push_str("bar");
+                    }
+                    appended_string
+                }
+            };
+            output.push(transformed_string);
         }
         output
     }
@@ -45,7 +55,7 @@ mod my_module {
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    use ???;
+    use super::my_module::transformer;
     use super::Command;
 
     #[test]
